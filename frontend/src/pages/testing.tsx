414be8b43dbf1f2997bc75653ad1f0bbb3b393e9
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 13:03:09 +0530

    added UI

--- frontend/src/pages/testing.tsx
+++ frontend/src/pages/testing.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const testing = () => {
-  return <div>hh </div>;
+  return (
+    <div className='mt-10 '>
+      <div className='flex justify-center'>testing</div>
+      <div className='flex justify-around mt-10'>
+        <div>niche wala 1</div>
+        <div>niche wala 2 </div>
+      </div>
+    </div>
+  );
 };
 
 export default testing;
