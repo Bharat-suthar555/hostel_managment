f45cb6b6c7dc0334935614eee6a3bbdd6c38ebc1
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 17:18:08 +0530

    fixed

--- frontend/src/pages/api/auth/[...nextauth].tsx
+++ frontend/src/pages/api/auth/[...nextauth].tsx
@@ -4,7 +4,6 @@ import GithubProvider from 'next-auth/providers/github';
 export const NextAuthOptions = {
   providers: [
     GithubProvider({
-      // clientId: 'c9c8c43f0c9bb42acfcc',
       clientId: 'c9c8c43f0c9bb42acfcc',
       clientSecret: '14aaf9fa45493fa339fda17abe79853bea2cbfba',
     }),
