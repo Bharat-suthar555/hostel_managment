cb5761b48202df2095224c40e16eaf26168ba8ab
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 18:06:39 +0530

    added interface

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -11,29 +11,36 @@ export default function Home() {
 
   if (!session.data) {
     return (
-      <div className='flex justify-end mt-5 mr-3'>
-        <button
-          onClick={() => signIn('github')}
-          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-        >
-          Sign in with GitHub
-        </button>
+      <div>
+        <div className='flex justify-end mt-5 mr-3'>
+          <button
+            onClick={() => signIn('github')}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          >
+            Sign in with GitHub
+          </button>
+        </div>
       </div>
     );
   }
 
   return (
     <div>
-      <div className='flex justify-center mt-5'>
-        <h1 className='text-4xl font-bold'>Welcome to NextAuth</h1>
-      </div>
-      <div className='flex justify-center mt-5'>
+      <div className='flex justify-end mt-5 mr-3'>
         <button
           onClick={() => signOut()}
           className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
         >
-          Sign in with GitHub
+          LogOut
         </button>
+      </div>
+      <div className='flex justify-center mt-10'>
+        <h1 className='text-4xl font-bold'>
+          Welcome to NextAuth {session?.data?.user?.name}
+        </h1>
+      </div>
+      <div className='flex justify-center mt-3'>
+        {session?.data?.user?.email}
       </div>
     </div>
   );
