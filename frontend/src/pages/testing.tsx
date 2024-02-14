1e41b5283f8537ae9c028243ef5b2d46a1d90359
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 13:13:45 +0530

    change Word

--- frontend/src/pages/testing.tsx
+++ frontend/src/pages/testing.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const testing = () => {
   return (
     <div className='mt-10 '>
-      <div className='flex justify-center'>testing</div>
+      <div className='flex justify-center'>Above Main Div</div>
       <div className='flex justify-around mt-10'>
-        <div>niche wala 1</div>
-        <div>niche wala 2 </div>
+        <div>Below First Div</div>
+        <div>Below Second Div</div>
       </div>
     </div>
   );
