970e434f8600e53d8fe29a7a750af888410f5d1e
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 13:25:42 +0530

    added technoHostel File

---
+++ frontend/src/pages/techno.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const technoHostel = () => {
+  return (
+    <div className='mt-10 '>
+      <div className='flex justify-center'>Above Main Div</div>
+      <div className='flex justify-around mt-10'>
+        <div>Below First Div</div>
+        <div>Below Second Div</div>
+      </div>
+    </div>
+  );
+};
+
+export default technoHostel;
