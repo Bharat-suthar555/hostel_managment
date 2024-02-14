c66740715f69d8917b4884431a58b0287d74e2a8
Bharat <bharatsuthar1632@gmail.com>
2024-02-14 17:17:36 +0530

    added nextAuth

---
+++ frontend/src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      // clientId: 'c9c8c43f0c9bb42acfcc',
+      clientId: 'c9c8c43f0c9bb42acfcc',
+      clientSecret: '14aaf9fa45493fa339fda17abe79853bea2cbfba',
+    }),
+  ],
+};
+
+export default NextAuth(NextAuthOptions);
